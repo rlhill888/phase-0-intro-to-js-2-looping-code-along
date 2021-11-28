63d1fc1646ce9cab0adb01b37bacfc7471888a49
RodneyHill <rlhill888@gmail.com>
2021-11-28 03:44:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+names = ["ally","d", "james"];
+function writeCards( names, event ) {
+    let thankYouCards = [];
+    for ( let i = 0; i < names.length; i++ ) {
+      thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    console.log(thankYouCards);
+    return thankYouCards
+    
+  }
+  writeCards(names, "birthday");
+ 
+function countDown(number){
+    while( number != -1){
+        console.log(number);
+        number = number -1;
+    }
+    return number
+
+}
+countDown(10);
+
